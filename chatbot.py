cf22abd95e590112283b1abd279080ed2b7c84c7
sudeeptech <94739504+sudeeptech@users.noreply.github.com>
2026-02-25 23:12:33 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -6,6 +6,7 @@ from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
+
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
